8855c26dadbccdb224954ec53837b935a56efd2f
alumno <raulplaza208@gmail.com>
2025-01-14 20:24:07 +0100

    Mostrando datos mediante el controlador

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -10,5 +10,6 @@ class TareasController extends Controller
         $mensaje = 'Lectura de Mensajes';
         $mensajes = ['mensaje1', 'mensaje2', 'mensaje3'];
         return view('mistareas', ['aviso' => $mensaje, 'mensajes' => $mensajes] );
+        //return view('mistareas', compact('aviso', 'mensajes')); (Es lo mismo que lo de arriba)
     }
 }
